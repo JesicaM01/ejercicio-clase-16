62e103684591cfcca5978994f589422fc28174ab
¨JesicaM01¨ <jesymerep@gmail.com>
2025-09-13 23:04:35 -0300

    ejercicio2

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,4 +1,10 @@
-const  numeros = [2,4,5,6,];
+//const  numeros = [2,4,5,6,];
+//console.log(numeros) ;
+//const cuadradoDenumeros = numeros.map(function(num){ return num * num; });
+//console.log(cuadradoDenumeros); 
+
+
+const numeros = [1,2,3,4,5,6,];
 console.log(numeros) ;
-const cuadradoDenumeros = numeros.map(function(num){ return num * num; });
-console.log(cuadradoDenumeros); 
+const numerosPares = numeros.filter(function(num){ return num % 2 === 0; });
+console.log(numerosPares);
