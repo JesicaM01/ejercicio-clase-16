a8133667333ad50a5cab5f2ee4f689a9b62a6e44
¨JesicaM01¨ <jesymerep@gmail.com>
2025-09-13 23:24:53 -0300

    ejercicio3

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,7 +4,13 @@
 //console.log(cuadradoDenumeros); 
 
 
-const numeros = [1,2,3,4,5,6,];
+//const numeros = [1,2,3,4,5,6,];
+//console.log(numeros) ;
+//const numerosPares = numeros.filter(function(num){ return num % 2 === 0; });
+//console.log(numerosPares);
+
+const numeros = [5,8,12,20,3];
 console.log(numeros) ;
-const numerosPares = numeros.filter(function(num){ return num % 2 === 0; });
-console.log(numerosPares);
+
+const numero = numeros.find(function(num) { return num > 10; });
+console.log(numero);
